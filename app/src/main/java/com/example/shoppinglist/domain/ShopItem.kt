f4ea2e070748370827be58296d1bdcb0cad64aa0
Vladislav <RubashkinVlad@yandex.ru>
2023-06-06 06:48:31 +0300

    Data layer

--- app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -3,20 +3,26 @@ package com.example.shoppinglist.domain
 /**
     TODO#1
 
+    Domain-слой - бизнес логика
+
     God-object - объект, который делает всё(напр. Activity внутри которой происходит загрузка из сети, добавление в БД
-    и отображение данных) - НЕДОПУСТИМО в чистой архитектуре
+    и отображение данных) - НЕДОПУСТИМО в чистой архитектуре.
 
     Неразрывно с чистой архитектурой идут принципы SOLID.
-    S - single responsibility(принцип единой ответственности) - класс должен отвечать только за что-то одно
+    S - single responsibility(принцип единой ответственности) - класс должен отвечать только за что-то одно.
 
     В чистой архитектуре - для каждого метода создаётся отдельный класс.
     Каждое действие бизнес логики, которое может вызвать пользователь, называется Interactor(UseCase).
-    Interactor(UseCase) - класс, который отвечает за одно действие в бизнес логике
+    Interactor(UseCase) - класс, который отвечает за одно действие в бизнес логике.
  */
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Double,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
